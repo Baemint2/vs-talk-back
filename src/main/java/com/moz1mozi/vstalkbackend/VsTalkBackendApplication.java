cbb377acf6001f952b22283403e485e44812b25c
Baemint2 <qofndlwl@gmail.com>
2025-07-14 21:59:04 +0900

    initial commit

---
+++ src/main/java/com/moz1mozi/vstalkbackend/VsTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moz1mozi.vstalkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsTalkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsTalkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moz1mozi/vstalkbackend/VsTalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moz1mozi.vstalkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsTalkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
