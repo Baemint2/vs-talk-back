76fe8e3f97632e2361eb961b4f4a4e826fe0b111
Baemint2 <qofndlwl@gmail.com>
2025-07-24 22:51:05 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/moz1mozi/vstalkbackend/VsTalkBackendApplication.java
+++ src/main/java/com/moz1mozi/vstalkbackend/VsTalkBackendApplication.java
@@ -2,8 +2,10 @@ package com.moz1mozi.vstalkbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VsTalkBackendApplication {
 
     public static void main(String[] args) {
